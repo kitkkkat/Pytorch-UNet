5d5d33b93be41fa3d5bf49127f3a0389f6a2ad1c
kitkkkat <138306696+kitkkkat@users.noreply.github.com>
2023-07-02 16:51:51 +0300

    Update train.py

--- train.py
+++ train.py
@@ -18,8 +18,8 @@ from unet import UNet
 from utils.data_loading import BasicDataset, CarvanaDataset
 from utils.dice_score import dice_loss
 
-dir_img = Path('./data/imgs/')
-dir_mask = Path('./data/masks/')
+#dir_img = Path('./data/imgs/')
+#dir_mask = Path('./data/masks/')
 dir_checkpoint = Path('./checkpoints/')
 
 
